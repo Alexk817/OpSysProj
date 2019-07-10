54c18c823909864610ef89299a7aee8b0db76d72
Jack Cherella <jcherella10@gmail.com>
2019-07-10 17:25:49 -0400

    updated random

--- main.cpp
+++ main.cpp
@@ -13,16 +13,44 @@
 
 std::vector<process> getTimes(int num_processes, int seed, double lambda, int upper_bound){
   srand48(seed);
+  double r,x;
+  std::vector<process> processes;
   for(int i=0; i<num_processes;i++){
     process temp;
-    temp.name = "A" + i;
-    double r = drand48();
-    int num_bursts = r % 100;
+    temp.name = 'A' + i;
+    r = drand48();
+    x = -log( r ) / lambda;
+    while(x > upper_bound){
+      r = drand48();
+      x = -log( r ) / lambda;  
+    }
+    temp.arrival = floor(x);
+    r = drand48();
+    int num_bursts = floor(r * 100);
     std::vector<std::pair<int, int>> burst_times;
     for(int j=0; j<num_bursts;j++){
-
+      std::pair<int, int> temp_pair;
+      r = drand48();
+      x = -log( r ) / lambda;
+      while(x > upper_bound){
+        r = drand48();
+        x = -log( r ) / lambda;  
+      }
+      temp_pair.first = ceil(x);
+      r = drand48();
+      x = -log( r ) / lambda;
+      while(x > upper_bound){
+        r = drand48();
+        x = -log( r ) / lambda;  
+      }
+      temp_pair.second = ceil(x);
+      burst_times.push_back(temp_pair);
     }
-  } 
+    temp.CPU_bursts = burst_times;
+    processes.push_back(temp);
+  }
+
+  return processes;
 }
 
 int main(int argc, char const *argv[]) {
@@ -45,5 +73,10 @@ int main(int argc, char const *argv[]) {
 	if (argc == 9) {
 		rr_add = argv[8];
 	}
+
+  std::vector<process> test = getTimes(2, 5, .001, 3000);
+  std::cout << test[0].name << ": " << test[0].arrival << "\n";
+  std::cout << test[1].name << ": " << test[1].arrival << "\n";
+
 	return EXIT_SUCCESS;
 }
