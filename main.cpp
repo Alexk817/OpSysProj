3827b571d48ed8c75c5f957c2c85c62290c73a97
Jack Cherella <jcherella10@gmail.com>
2019-07-12 18:15:00 -0400

    Print statement to genProc

--- main.cpp
+++ main.cpp
@@ -59,6 +59,10 @@ std::vector<Process> generateProcesses(int num_processes, int seed, double lambd
 		Process temp(name, arrival, burst_times,ceil(1/lambda));
 		processes.push_back(temp);
 	}
+
+	for(int i=0; i < num_processes; i++){
+		std::cout << "Process " << processes[i].name << " [NEW] (arrival time " << processes[i].arrival << "ms) " << processes[i].CPU_bursts.size() << " CPU bursts\n";
+	}
 	return processes;
 }
 
@@ -125,5 +129,9 @@ int main(int argc, char const *argv[]) {
 		rr_add = std::string(argv[8]);
 	}
 	
+	std::vector<Process> test = generateProcesses(2, 5, .001, 3000);
+  std::cout << test[0].name << ": " << test[0].arrival << "\n";
+  std::cout << test[1].name << ": " << test[1].arrival << "\n";
+
 	return EXIT_SUCCESS;
 }
