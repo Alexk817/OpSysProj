1c111e3d1226326f419069f26955cf613848ca16
Alexk817 <alexanderkim878@gmail.com>
2019-07-11 21:09:52 -0400

    Formatting

--- Process.cpp
+++ Process.cpp
@@ -9,25 +9,5 @@ Process::Process(char aName, int aArrival, std::vector<std::pair<int, int> > aCP
     arrival = aArrival;
     CPU_bursts = aCPUBursts;
     status = "READY";
-    tau = initTau
-}
-
-char Process::getName() const {
-    return name;
-}
-
-int Process::getArrival() const {
-	return arrival;
-}
-
-std::vector<std::pair<int, int> > Process::getCPUBursts() const {
-    return CPU_bursts;
-}
-
-std::string Process::getStatus() const {
-    return status;
-}
-
-void Process::setStatus(std::string newStatus) {
-    status = newStatus;
+    tau = initTau;
 }
