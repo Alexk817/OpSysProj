102507528f323603b21cafd3108e565b65b89a6a
Jack Cherella <jcherella10@gmail.com>
2019-07-11 23:13:39 -0400

    Fixed generateProcesses to not have a final io burst

--- main.cpp
+++ main.cpp
@@ -11,54 +11,58 @@
 #include "Taufunc.h"
 
 /* 
-  Generate a vector of processes that the CPU scheduling algorithms with use
-  Each process with have a vector of CPU Bursts of size 1-100
-  Each Burst will have a CPU Burst time and IO Burst time
+	Generate a vector of processes that the CPU scheduling algorithms with use
+	Each process with have a vector of CPU Bursts of size 1-100
+	Each Burst will have a CPU Burst time and IO Burst time
 */
 std::vector<Process> generateProcesses(int num_processes, int seed, double lambda, int upper_bound) {
-  srand48(seed);
-  double r,x;
-  std::vector<Process> processes;
-  char name;
-  for(int i = 0; i < num_processes; i++) {
-    name = 'A' + i;
-    r = drand48();
-    x = -log( r ) / lambda;
-    while(x > upper_bound) {
-      r = drand48();
-      x = -log( r ) / lambda;  
-    }
-    int arrival = floor(x);
-    r = drand48();
-    int num_bursts = floor(r * 100);
-    std::vector<std::pair<int, int> > burst_times;
-    for(int j = 0; j < num_bursts; j++) {
-      std::pair<int, int> temp_pair;
-      r = drand48();
-      x = -log( r ) / lambda;
-      while(x > upper_bound) {
-        r = drand48();
-        x = -log( r ) / lambda;  
-      }
-      temp_pair.first = ceil(x);
-      r = drand48();
-      x = -log( r ) / lambda;
-      while(x > upper_bound) {
-        r = drand48();
-        x = -log( r ) / lambda;  
-      }
-      temp_pair.second = ceil(x);
-      burst_times.push_back(temp_pair);
-    }
-    Process temp(name, arrival, burst_times,ceil(1/lambda));
-    processes.push_back(temp);
-  }
+	srand48(seed);
+	double r,x;
+	std::vector<Process> processes;
+	char name;
+	for(int i = 0; i < num_processes; i++) {
+		name = 'A' + i;
+		r = drand48();
+		x = -log( r ) / lambda;
+		while(x > upper_bound) {
+			r = drand48();
+			x = -log( r ) / lambda;  
+		}
+		int arrival = floor(x);
+		r = drand48();
+		int num_bursts = floor(r * 100);
+		std::vector<std::pair<int, int> > burst_times;
+		for(int j = 0; j < num_bursts; j++) {
+			std::pair<int, int> temp_pair;
+			r = drand48();
+			x = -log( r ) / lambda;
+			while(x > upper_bound) {
+				r = drand48();
+				x = -log( r ) / lambda;  
+			}
+			temp_pair.first = ceil(x);
+			r = drand48();
+			x = -log( r ) / lambda;
+			while(x > upper_bound) {
+				r = drand48();
+				x = -log( r ) / lambda;  
+			}
+			if(j != num_bursts-1){
+				temp_pair.second = ceil(x);
+			}else{
+				temp_pair.second = -1;
+			}
+			burst_times.push_back(temp_pair);
+		}
+		Process temp(name, arrival, burst_times,ceil(1/lambda));
+		processes.push_back(temp);
+	}
 
-  return processes;
+	return processes;
 }
 
 int main(int argc, char const *argv[]) {
-  // Reading in Arguments
+	// Reading in Arguments
 	if (argc != 8 && argc != 9) {
 		std::cerr << "Invalid number of argument" << std::endl;
 		return EXIT_FAILURE;
