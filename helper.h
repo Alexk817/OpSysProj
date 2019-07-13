4594b4498f30b3d6f549bcb2702276ee4524bdab
Alexk817 <alexanderkim878@gmail.com>
2019-07-13 17:39:24 -0400

    moved functions to helper

--- helper.h
+++ helper.h
@@ -17,4 +17,16 @@ void printEvent(int time, std::string detail, std::vector<Process*> que);
 // Calculate average CPU burst time
 double calcAvgCPUBurst(std::vector<Process> &processes);
 
+// Increment wait times for processes in the vector
+void incWaitTime(std::vector<Process*> &ready_queue);
+
+// Check if processes have arrived
+void addArived(std::vector<Process> &processes,std::vector<Process*> &ready_queue,int &curr_time);
+
+// Pop the element from ready queue if available
+void popQueifPossible(std::vector<Process*> &ready_queue, Process* &curr_process, int &curr_time ,std::vector<Process> &processes, int &context_time,char* buff );
+
+// Caclulate the average of a vector
+double calcAvgVal(std::vector<int> times);
+
 #endif
